36b92d61c2d0b7896f86b93a390def071d18c21d
Yana <yana.kod01@gmail.com>
2023-04-21 16:37:31 +0300

    -second commit of lab2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,82 +5,63 @@ import java.util.Scanner;
 
 public class Main {
     static int row, column;
+    static Scanner sc = new Scanner(System.in);
     static final int N = 100;
     static Random random = new Random();
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter size of the array!");
         System.out.print("Enter row: ");
         row = sc.nextInt();
         System.out.print("Enter column: ");
         column = sc.nextInt();
         sc.nextLine();
 
-        sc.close();
-
         int [][] board = new int [row][column];
 
-        /*
-        System.out.println("Choose how you want to set the size of the array");
-        System.out.println("1: Random size");
-        System.out.println("2: Enter size");
-        String menuNumber = sc.nextLine();
-        switch (menuNumber) {
-            case "1":
-                RandomSizeArray();
-                break;
-            case "2":
-                YourOwnSizeArray();
-                break;
-            default:
-                System.out.println("Command not recognise");
-        }
-         */
-
-
-
-        String n = Menu1();
-        while(!n.equals("1") && !n.equals("2") && !n.equals("3")) {
-            System.out.println("Command not recognise");
-            n = Menu1();
-        }
-        switch (n) {
-            case "1":
-                RandomArray(board);
-                printArray(board);
-                break;
-            case "2":
-                YourOwnArray(board);
-                break;
-            case "3":
-                break;
-            default:
-                System.out.println("Command not recognise");
-        }
+        Menu1(board);
+        Menu2(board);
+    }
+    private static void Menu2(int[][] arr) {
         String m;
         do {
-            m = Menu2();
+            m = selectMenu2();
             switch (m) {
                 case "1":
-                    System.out.println("Min = " + minimumValue(board));
+                    System.out.println("Min = " + minimumValue(arr));
                     break;
                 case "2":
-                    System.out.println("Min = " + maximumValue(board));
+                    System.out.println("Min = " + maximumValue(arr));
                     break;
                 case "3":
-                    System.out.println("Average = " + AverageValue(board));
+                    System.out.println("Average = " + AverageValue(arr));
                     break;
                 case "4":
                     break;
                 default:
-                    System.out.println("Command not recognise");
+                    System.out.println("Command not recognise! Try again!");
             }
         }while(!m.equals("4"));
     }
-    private static String Menu2() {
-        Scanner scanner = new Scanner(System.in);
+    private static void Menu1(int[][] arr){
+        String n = selectMenu1();
+        while(!n.equals("1") && !n.equals("2")) {
+            System.out.println("Command not recognise! Try again!");
+            n = selectMenu1();
+        }
+        switch (n) {
+            case "1":
+                RandomArray(arr);
+                break;
+            case "2":
+                YourOwnArray(arr);
+                break;
+            default:
+                System.out.println("Command not recognise! Try again!");
+        }
+    }
+    private static String selectMenu2() {
 
         System.out.println("Choose what you`d like to do");
         System.out.println("1: Find the minimum value");
@@ -88,26 +69,18 @@ public class Main {
         System.out.println("3: Find the average value of the array");
         System.out.println("4: Exit");
 
-        String menuNumber2 = scanner.nextLine();
-        scanner.close();
-
+        String menuNumber2 = sc.nextLine();
         return menuNumber2;
     }
-
-    private static String Menu1() {
-        Scanner scanner = new Scanner(System.in);
+    private static String selectMenu1() {
 
         System.out.println("Choose how you want initialize the array!");
         System.out.println("1: Random numbers");
         System.out.println("2: Enter numbers");
-        System.out.println("3: Exit");
 
-        String menuNumber1 = scanner.nextLine();
-        scanner.close();
-
+        String menuNumber1 = sc.nextLine();
         return menuNumber1;
     }
-
     private static double AverageValue(int[][] arr) {
 
         double sum = 0.0;
@@ -149,42 +122,15 @@ public class Main {
             {
                 System.out.print("a[" + i + "][" + j + "] : ");
                 arr[i][j] = sc.nextInt();
-                System.out.println();
             }
-        sc.close();
         printArray(arr);
     }
-    private static int[][] RandomArray(int[][] arr) {
+    private static void RandomArray(int[][] arr) {
         for(int i = 0; i < row; i++)
             for(int j = 0; j < column; j++)
                 arr[i][j] = random.nextInt(N);
-        return arr;
+        printArray(arr);
     }
-    /*private static void YourOwnSizeArray() {
-        System.out.println("Max size of the array 20x20");
-        System.out.print("Enter number of row: ");
-        do {
-            row = sc.nextInt();
-            if(row > 20) {
-                System.out.println("You entered wrong number");
-                System.out.print("Enter number of row: ");
-            }
-        }while (row > 20);
-        System.out.print("Enter number of column: ");
-        do {
-            column = sc.nextInt();
-            if(column > 20) {
-                System.out.println("You entered wrong number");
-                System.out.print("Enter number of column: ");
-            }
-        }while (column > 20);
-    }
-    private static void RandomSizeArray() {
-        row = random.nextInt(SIZE);
-        column= random.nextInt(SIZE);
-    }
-
-     */
     private static void printArray(int[][] arr) {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++)
@@ -192,6 +138,4 @@ public class Main {
             System.out.println();
         }
     }
-
-
 }
