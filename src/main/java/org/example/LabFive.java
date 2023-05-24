06cc1330c5a4720371be39d6a0a3c1093c20a3b3
Yana <yana.kod01@gmail.com>
2023-05-24 20:09:17 +0300

    -second commit lab5

--- src/main/java/org/example/LabFive.java
+++ src/main/java/org/example/LabFive.java
@@ -2,12 +2,20 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class LabFive {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+        public static void main(String[] args) {
+            String url = "jdbc:sqlserver://localhost;encrypt=true;instanceName=DONKEY\\SQLEXPRESS;databaseName=students;integratedSecurity=true;";
+            try {
+                DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+                try (Connection conn = DriverManager.getConnection(url)) {
+                    System.out.println("Got it!");
+                }
+            }
+            catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
 }
